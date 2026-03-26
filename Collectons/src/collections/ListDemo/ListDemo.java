b04800163241c98841665ea931e5253b716740f6
Protyush <mdey7054@gmail.com>
2026-03-27 02:20:19 +0530

    ArrayList

---
+++ Collectons/src/collections/ListDemo/ListDemo.java
@@ -0,0 +1,16 @@
+package collections.ListDemo;
+
+import java.util.*;
+
+public class ListDemo {
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList<Integer>(); // here we need to use wraper class
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        a.add(5);
+        a.add(6);
+        //the arraylist is not size specific
+    }
+}
