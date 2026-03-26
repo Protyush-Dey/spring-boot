985c955f71ced7422b58530afa80aa5a61db135e
Protyush <mdey7054@gmail.com>
2026-03-27 00:55:36 +0530

    collection folder and wraper class

---
+++ Collectons/src/WraperClass/MethodDemo.java
@@ -0,0 +1,17 @@
+package WraperClass;
+
+public class MethodDemo {
+    public static void main(String[] args){
+        String s = "123";
+        int a = Integer.parseInt((s)); //convert string to int
+
+
+        int n = 10;
+        String st = Integer.toString(a); //convert iny to string
+
+        Integer s1 = 10;
+        Integer s2 = 10;
+
+        System.out.println("compare " +Integer.compare(s1,s2));
+    }
+}
---
+++ Collectons/src/WraperClass/WraperDemo.java
@@ -0,0 +1,10 @@
+package WraperClass;
+
+public class WraperDemo {
+    public static void main(String[] args){
+        int a = 10;
+        Integer i = a; // autoboxing -> premitive to wraper
+        int b = i; // unboxing -> // autoboxing -> wraper to premitive
+        System.out.println(i);
+    }
+}
